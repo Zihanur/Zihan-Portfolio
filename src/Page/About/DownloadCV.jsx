be77171f778ff1210ea6ec43be89991244ef0529
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-07-02 23:17:16 +0600

    Updated resume pdf download

--- src/Page/About/DownloadCV.jsx
+++ src/Page/About/DownloadCV.jsx
@@ -1,26 +1,20 @@
-import { saveAs } from 'file-saver';
 import { motion } from "framer-motion";
-import { AiOutlineFilePdf } from 'react-icons/ai';
+import { AiOutlineFilePdf } from "react-icons/ai";
 
-const pdfUrl = "http://localhost:5173/zihan-resume.pdf";
-
+const link = "https://drive.google.com/uc?export=download&id=1pvWn_vNhk588ezX-ZKmpqadZsWgSdeg9"
 const DownloadCV = () => {
+  const handleDownload = () =>{
+    window.open(link, "_blank")
+  }
 
-  const downloadPdf = () => {
-    fetch(pdfUrl)
-    .then((response) => response.blob())
-    .then((blob) => {
-      saveAs(blob, 'Zihanur_Resume.pdf');
-    })
-    .catch((error) => {
-      console.error('Error downloading the PDF:', error);
-    });
-  };
-  
   return (
     <motion.button
-      whileHover={{scale:1.1,textShadow:"0px 0px 4px rgb(255,255,255)",boxShadow:"0px 0px 8px rgb(255,255,255)"}}
-      onClick={downloadPdf}
+      whileHover={{
+        scale: 1.1,
+        textShadow: "0px 0px 4px rgb(255,255,255)",
+        boxShadow: "0px 0px 8px rgb(255,255,255)",
+      }}
+      onClick={handleDownload}
       className="bg-slate-900 text-white text-2xl font-medium py-3 px-4 rounded-md flex items-center gap-2"
     >
       Download CV <AiOutlineFilePdf></AiOutlineFilePdf>
